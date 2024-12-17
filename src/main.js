e1f6b734dba3babf570c820e872207ec6d512f28
Izosimov_Andrew <chernigovsky108@gmail.com>
2024-12-17 18:53:53 +0300

    swipers

--- src/main.js
+++ src/main.js
@@ -3,11 +3,14 @@ import { setupCounter } from './js/counter.js'
 import { initTabs } from './js/tabs.js'
 import { hotels } from "./js/hotels.js";
 import { generateHotels } from './js/generate-hotels.js';
+import { initSwipers } from './js/swiper.js';
 
 const counter = document.querySelector('.counter');
 if (counter) {
   setupCounter(counter);
 }
 
-setTimeout(() => generateHotels(hotels), 1000)
+// generateHotels(hotels);
 initTabs();
+
+document.addEventListener('DOMContentLoaded', initSwipers);
